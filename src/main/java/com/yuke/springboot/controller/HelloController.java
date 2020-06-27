336a46bfc3d4b6ebd4a4feaf06c0c3013cc2b00c
yuke <yuke198907@126.com>
2020-06-27 16:18:50 +0800

    logback日志配置及测试

--- src/main/java/com/yuke/springboot/controller/HelloController.java
+++ src/main/java/com/yuke/springboot/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.yuke.springboot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String hello(){
-        return "Hello World";
+    public String hello(String str){
+        log.info("/hello,{}", str);
+        return "Hello World,"+str;
     }
 }
