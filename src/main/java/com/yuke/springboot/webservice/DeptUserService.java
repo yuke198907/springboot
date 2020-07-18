2d51bec93475fd48a58ead5ca1e1cb9ad47fb1ab
yuke <yuke198907@126.com>
2020-07-18 21:38:14 +0800

    webservice

---
+++ src/main/java/com/yuke/springboot/webservice/DeptUserService.java
@@ -0,0 +1,35 @@
+package com.yuke.springboot.webservice;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+import javax.xml.ws.soap.SOAPBinding;
+
+@WebService(targetNamespace = "http://GetUserInfoService.com/")
+@BindingType(value = SOAPBinding.SOAP11HTTP_BINDING)
+public interface DeptUserService {
+
+    public static String FAILURE = "1";
+    public static String SUCCESS = "0";
+
+    /**
+     * 更新各应用系统的用户信息
+     * @param model
+     * @param userInfo
+     * @return
+     */
+    @WebMethod(operationName = "UpdateUserInfo", action = "urn:anonOutInOp")
+    public String updateUserInfo(@WebParam(name = "Mode", targetNamespace = "http://GetUserInfoService.com/")String model,//
+                                 @WebParam(name = "UserInfo", targetNamespace = "http://GetUserInfoService.com/")String userInfo);
+
+    /**
+     * 更新各应用系统的组织信息
+     * @param model
+     * @param deptInfo
+     * @return
+     */
+    @WebMethod(operationName = "UpdateBranchInfo", action = "urn:anonOutInOp")
+    public String updateDeptInfo(@WebParam(name = "Mode", targetNamespace = "http://GetUserInfoService.com/")String model,//
+                                 @WebParam(name = "UserInfo", targetNamespace = "http://GetUserInfoService.com/")String deptInfo);
+}
