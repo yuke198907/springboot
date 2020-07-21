b25dc400dfc5880dedd0f285254ee9146b66b9a1
yuke <yuke198907@126.com>
2020-07-21 21:39:47 +0800

    webservice

---
+++ src/main/java/com/yuke/springboot/webservice/BeforeStartUp.java
@@ -0,0 +1,32 @@
+package com.yuke.springboot.webservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Service;
+
+import javax.xml.ws.Endpoint;
+
+@Slf4j
+@Service
+public class BeforeStartUp implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Value("${config.webservice.deptuser}")
+    private String address;
+
+    @Autowired
+    private DeptUserService deptUserService;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if(deptUserService == null){
+            log.info("================================================null");
+        }
+
+        Endpoint.publish(address, deptUserService);
+        log.info("webService 服务发布成功！！！");
+        log.info("wsdl地址："+address+"?wsdl");
+    }
+}
