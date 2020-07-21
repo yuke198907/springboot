bd034dd0d519cd80358ed5b65f505e8828979787
yuke <yuke198907@126.com>
2020-07-21 21:39:38 +0800

    webservice

---
+++ src/main/java/com/yuke/springboot/webservice/DeptUserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.yuke.springboot.webservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.jws.WebService;
+
+@Slf4j
+@Service
+@WebService(
+        endpointInterface = "com.yuke.springboot.webservice.DeptUserService",
+        targetNamespace = "http://GetUserInfoService.com/"
+)
+public class DeptUserServiceImpl implements DeptUserService {
+
+    @Override
+    public String updateUserInfo(String model, String userInfo) {
+        log.info("接受到更新数据updateUserInfo 开始");
+        log.info("model = " +model + "   userInfo = " + userInfo);
+
+
+        return "1";
+    }
+
+    @Override
+    public String updateDeptInfo(String model, String deptInfo) {
+        log.info("deptInfo 开始");
+        log.info("model = " +model + "   deptInfo = " + deptInfo);
+        return "1";
+    }
+}
