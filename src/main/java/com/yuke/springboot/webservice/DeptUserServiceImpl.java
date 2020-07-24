36561ad38a0fb08ae67123190031ce3c8e996658
yuke <yuke198907@126.com>
2020-07-24 21:42:31 +0800

    webservice

--- src/main/java/com/yuke/springboot/webservice/DeptUserServiceImpl.java
+++ src/main/java/com/yuke/springboot/webservice/DeptUserServiceImpl.java
@@ -17,7 +17,7 @@ public class DeptUserServiceImpl implements DeptUserService {
     public String updateUserInfo(String model, String userInfo) {
         log.info("接受到更新数据updateUserInfo 开始");
         log.info("model = " +model + "   userInfo = " + userInfo);
-        return "1";
+        return "ok";
     }
 
     @Override
