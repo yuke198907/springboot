6c8416805ccdf70bbf0808e9bcaa90b1376f4c53
yuke <yuke198907@126.com>
2020-07-24 21:46:50 +0800

    servlet

--- src/main/java/com/yuke/springboot/SpringbootApplication.java
+++ src/main/java/com/yuke/springboot/SpringbootApplication.java
@@ -2,9 +2,10 @@ package com.yuke.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan("com.yuke.springboot.servlet")
 public class SpringbootApplication {
 
     public static void main(String[] args) {
